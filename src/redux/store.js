ac9b3df822167e8b8cd5681215c8c3cf4a692ac7
Vlad <vadya-85@mail.ru>
2024-06-27 10:09:47 +0200

    add persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,40 +1,34 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import {
-//     persistStore,
-//     persistReducer,
-//     FLUSH,
-//     REHYDRATE,
-//     PAUSE,
-//     PERSIST,
-//     PURGE,
-//     REGISTER,
-//   } from 'redux-persist'
-// import storage from 'redux-persist/lib/storage';
+import { configureStore } from '@reduxjs/toolkit';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
-// import { rootReducer } from './reducers'; 
+import { reducer } from './reducer';
 
-// const persistConfig = {
-//   key: 'contacts',
-//   storage,
-// };
 
-// const namedPersistReducer = persistReducer(persistConfig, rootReducer);
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
 
-// export const store = configureStore({
-//   reducer: {
-//     contacts: namedPersistReducer,//todo: add reducer here
-//   },
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
+const persistedReducer = persistReducer(persistConfig, reducer);
 
-// export const persistor = persistStore(store);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-import { reducer } from "./reducer";
-import { configureStore } from "@reduxjs/toolkit";
-
-export const store = configureStore({reducer});
+export const persistor = persistStore(store);
